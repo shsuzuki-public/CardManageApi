ae26a4c8346bb92e1e5a28aba4d3c0d64105b2dc
shsuzuki-public <45429752+shsuzuki-public@users.noreply.github.com>
2018-12-05 00:59:03 +0900

    初期配置

---
+++ RestApi/src/main/java/com/example/restApi/service/DeckMService.java
@@ -0,0 +1,12 @@
+package com.example.restApi.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class DeckMService {
+	@Autowierd
+
+}
