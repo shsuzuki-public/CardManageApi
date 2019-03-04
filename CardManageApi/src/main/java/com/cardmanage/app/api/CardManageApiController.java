56a173c51d13d7d3335bc5c4aa5d78703bee3f84
shsuzuki <shsuzuki.public@gmail.com>
2019-03-05 00:38:36 +0900

    #6 Controllerにpost追加

--- CardManageApi/src/main/java/com/cardmanage/app/api/CardManageApiController.java
+++ CardManageApi/src/main/java/com/cardmanage/app/api/CardManageApiController.java
@@ -1,17 +1,27 @@
 package com.cardmanage.app.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.cardmanage.app.domain.ClanM;
 import com.cardmanage.app.domain.DeckM;
 import com.cardmanage.app.service.ClanMService;
 import com.cardmanage.app.service.DeckMService;
+
+import lombok.val;
 
 @RestController
 public class CardManageApiController {
@@ -21,6 +31,8 @@ public class CardManageApiController {
 	ClanMService clanMService;
 
 	@RequestMapping(value="/clans", method=RequestMethod.GET)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
 	public List<ClanM> getClanList(){
 		return clanMService.getAllClanList();
 	}
@@ -39,4 +51,14 @@ public class CardManageApiController {
 	public List<DeckM> getDeckDetailList(@PathVariable("deckId") Integer deckId){
 		return deckMService.getDeckDetail(deckId);
 	}
+
+	@RequestMapping(value="/decks/deckregist", method=RequestMethod.POST)
+	public ResponseEntity<DeckM> registDeck(@ModelAttribute DeckM deckM, UriComponentsBuilder uriComponentsBuilder){
+		List<DeckM> deckData = new ArrayList<DeckM>();
+		deckData.add(deckM);
+		val headers = new HttpHeaders();
+		headers.setLocation(uriComponentsBuilder.path("/decks/deckregist/{id})").buildAndExpand(deckData.size()).toUri());
+		return new ResponseEntity<DeckM>(deckM, headers, HttpStatus.CREATED);
+	}
+	
 }
